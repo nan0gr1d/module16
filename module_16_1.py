c90775acea1a1516787f445b6ff6d344cdbebd0e
Urban <prestofan@gmail.com>
2024-10-18 16:58:55 +0300

    Маршрутизация прописана.

---
+++ module_16_1.py
@@ -0,0 +1,21 @@
+#  module_16_1
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def get_main_page() -> str:
+    return 'Главная страница'
+
+@app.get('/user/admin')
+async def get_admin_page() -> str:
+    return 'Вы вошли как администратор'
+
+@app.get('/user/{user_id}')
+async def get_user_number(user_id: int) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
+
+@app.get('/user/{username}/{age}')
+async def get_user_info(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
